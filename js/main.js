dc592e6f1ec569bc5e76a2227d9aa44c218b6ba3
tdunn891 <thomas.dunn@uqconnect.edu.au>
2020-07-08 10:49:20 +0100

    Add EmailJS functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
 	$('.button-collapse').sideNav();
+	// Send Notification from EmailJS
+	emailjs
+		.send('portfolio_gmail_notification', 'portfolio_visitor_template')
+		.then(
+			function (response) {
+				console.log('EmailJS: SUCCESS!', response.status, response.text);
+			},
+			function (error) {
+				console.log('EmailJS: FAILED...', error);
+			}
+		);
 });
